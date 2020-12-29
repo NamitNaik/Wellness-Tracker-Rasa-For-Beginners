41e3d775668bf16d2aed91ea3e6b25e9ff89f09b
Namit Naik <namitnaik1999@gmail.com>
2020-12-29 10:28:08 +0530

    init

---
+++ actions/actions.py
@@ -0,0 +1,142 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+from typing import Any, Text, Dict, List, Union
+from dotenv import load_dotenv
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+
+import requests
+import json
+import os
+
+load_dotenv()
+
+airtable_api_key=os.getenv("AIRTABLE_API_KEY")
+base_id=os.getenv("BASE_ID")
+table_name=os.getenv("TABLE_NAME")
+
+def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
+    request_url=f"https://api.airtable.com/v0/{base_id}/{table_name}"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {airtable_api_key}",
+    }  
+    data = {
+        "fields": {
+            "Exercised?": confirm_exercise,
+            "Type of exercise": exercise,
+            "Amount of sleep": sleep,
+            "Stress": stress,
+            "Diet": diet,
+            "Goal": goal,
+        }
+    }
+    try:
+        response = requests.post(
+            request_url, headers=headers, data=json.dumps(data)
+        )
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+    
+    return response
+    print(response.status_code)
+
+class HealthForm(FormAction):
+
+    def name(self):
+        return "health_form"
+
+    @staticmethod
+    def required_slots(tracker):
+
+        if tracker.get_slot('confirm_exercise') == True:
+            return ["confirm_exercise", "exercise", "sleep",
+             "diet", "stress", "goal"]
+        else:
+            return ["confirm_exercise", "sleep",
+             "diet", "stress", "goal"]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+
+        return {
+            "confirm_exercise": [
+                self.from_intent(intent="affirm", value=True),
+                self.from_intent(intent="deny", value=False),
+                self.from_intent(intent="inform", value=True),
+            ],
+            "sleep": [
+                self.from_entity(entity="sleep"),
+                self.from_intent(intent="deny", value="None"),
+            ],
+            "diet": [
+                self.from_text(intent="inform"),
+                self.from_text(intent="affirm"),
+                self.from_text(intent="deny"),
+            ],
+            "goal": [
+                self.from_text(intent="inform"),
+            ],
+        }
+
+    def submit(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict]:
+
+        confirm_exercise = tracker.get_slot("confirm_exercise")
+        exercise = tracker.get_slot("exercise")
+        sleep = tracker.get_slot("sleep")
+        stress = tracker.get_slot("stress")
+        diet = tracker.get_slot("diet")
+        goal = tracker.get_slot("goal")
+
+        response = create_health_log(
+                confirm_exercise=confirm_exercise,
+                exercise=exercise,
+                sleep=sleep,
+                stress=stress,
+                diet=diet,
+                goal=goal
+            )
+
+        dispatcher.utter_message("Thanks, your answers have been recorded!")
+        return []
+
